6dcc0e209ff9882702f7846cb60b07cd6b344a4d
Gabriel Ribeiro <biel.grom@gmail.com>
2023-02-08 02:22:11 -0300

    add views client

---
+++ src/views/Clientes.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Gerenciar Clientes:</h1>
+      <ClientsDash />
+    </div>
+  </div>
+</template>
+
+<script>
+import ClientsDash from '../components/ClientsDash'
+
+export default {
+  name: 'Clientes',
+  components: {
+    ClientsDash,
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Cadastre seu cliente:</h1>
+      <ClientForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import ClientForm from '../components/ClientForm'
+
+export default {
+  name: 'Home',
+  components: {
+    ClientForm
+  }
+}
+</script>
