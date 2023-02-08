6c3fd5763d1976f75930abf01f85ea3a90a8361a
Gabriel Ribeiro <biel.grom@gmail.com>
2023-02-08 02:25:24 -0300

    update name app

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   data: function() {
     return {
       logo_src: "/img/logo.png",
-      app_name: "Make Your Burger"
+      app_name: "customers products"
     }
   },
   components: {
