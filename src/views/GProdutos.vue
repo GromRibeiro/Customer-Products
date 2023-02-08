1aacca2acf68b8306ce6c5331982f46f17171f00
Gabriel Ribeiro <biel.grom@gmail.com>
2023-02-08 02:22:27 -0300

    add views produtos

---
+++ src/views/GProdutos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Gerenciar Produtos:</h1>
+      <Dashboard />
+    </div>
+  </div>
+</template>
+
+<script>
+import Dashboard from '../components/Dashboard'
+
+export default {
+  name: 'GProdutos',
+  components: {
+    Dashboard
+  }
+}
+</script>
---
+++ src/views/Produtos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <div class="main-container">
+    <h1>Cadastre seu Produto:</h1>
+    <ProdutosForm />
+  </div>
+  </div>
+</template>
+
+<script>
+import ProdutosForm from '../components/ProdutosForm'
+
+export default {
+  name: 'Home',
+  components: {
+    ProdutosForm
+  }
+}
+</script>
