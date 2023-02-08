4b304c68498ed71048eb7358097d2d831b2a908a
Gabriel Ribeiro <biel.grom@gmail.com>
2023-02-08 02:23:11 -0300

    add footer/navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="footer">
+    <p>Gabriel Ribeiro &copy; 2023</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link id="logo-url" to="/">
+        <img id="logo" :src="logo" :alt="alt">
+      </router-link>
+      <router-link to="/">Cadastro Clientes</router-link>
+      <router-link to="/produtos">Cadastro Produtos</router-link>
+      <router-link to="/gprodutos">Produtos</router-link>
+      <router-link to="/clientes">Clientes</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Navbar",
+  props: ["logo", "alt"]
+}
+</script>
+
+<style scoped>
+  #nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  #nav #logo-url {
+    margin: auto;
+    margin-left: 0;
+  }
+
+  #logo {
+    width: 65px;
+    height: 40px;
+  }
+
+  #nav a {
+    color: #fcba03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .5s;
+  }
+
+  #nav a:hover {
+    color: #FFF;
+  }
+</style>
